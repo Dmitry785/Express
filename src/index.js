02e8d1ad34c64827bd419fbfada4be5da331bd5a
Dmitry785 <danchenko.d2020@mail.ru>
2026-08-13 23:16:44 +0300

    add handlebars view engine

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 const express = require('express');
 
 const _port = 3000;
-const _ip = "127.0.0.1";
+const _ip = "0.0.0.0";
 
 const app = express();
 
-
+app.set("view engine", "hbs");
 
 app.get('/', (req, res) => {
     res.end("Hello");
 });
 
+app.get('/contacts', (req, res) => {
+    res.render('contact.hbs', {
+        title: "Contacts page",
+        showList: false,
+        list: ["tag 1", "tag 2", "tag 3"]
+    });
+})
 
-app.listen(_port, _ip, () => console.log(`open on ${_ip}:${_port}`));
+
+app.listen(_port, _ip, () => console.log(`open on ${_ip}:${_port}\ndir: ${__dirname}`));
