c3ed5de833feaaa20e1cfbf2f0b0706f16c60c78
Dmitry785 <danchenko.d2020@mail.ru>
2026-08-14 06:22:21 +0300

    add partials views

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const hbs = require('hbs');
+const path = require("node:path")
 
 const _port = 3000;
 const _ip = "0.0.0.0";
 
 const app = express();
+hbs.registerPartials(path.join(__dirname, "..", "views/partials"));
 
 app.set("view engine", "hbs");
 
@@ -14,7 +17,7 @@ app.get('/', (req, res) => {
 app.get('/contacts', (req, res) => {
     res.render('contact.hbs', {
         title: "Contacts page",
-        showList: false,
+        showList: true,
         list: ["tag 1", "tag 2", "tag 3"]
     });
 })
