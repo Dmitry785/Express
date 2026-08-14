0434fa8d037b6141778c05eeb954490418620325
Dmitry785 <danchenko.d2020@mail.ru>
2026-08-14 06:48:51 +0300

    add loyout

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 const express = require('express');
 const hbs = require('hbs');
+const expressHbs = require('express-handlebars');
 const path = require("node:path")
 
 const _port = 3000;
 const _ip = "0.0.0.0";
 
 const app = express();
+
+app.engine("hbs", expressHbs.engine(
+    {
+        layoutsDir: "views/layouts",
+        defaultLayout: "layout",
+        extname: "hbs"
+    }
+))
+
 hbs.registerPartials(path.join(__dirname, "..", "views/partials"));
 
 app.set("view engine", "hbs");
 
 app.get('/', (req, res) => {
-    res.end("Hello");
+    res.render("home");
 });
 
-app.get('/contacts', (req, res) => {
-    res.render('contact.hbs', {
+app.use('/contacts', (_, res) => {
+    res.render('contact', {
         title: "Contacts page",
         showList: true,
         list: ["tag 1", "tag 2", "tag 3"]
